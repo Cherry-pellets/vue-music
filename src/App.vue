df5a7ae71eb4ec85ce716c2d4774fa3eab6818dc
hjx <50487156+Cherry-pellets@users.noreply.github.com>
2020-05-03 11:53:06 +0800

    tabbar组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Tabbar></Tabbar>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tabbar from './components/Tabbar'
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Tabbar
   },
   methods: {
   }
---
+++ src/components/Tabbar.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="tabbar">
+     <router-link tag="div" class="item" to="/recommend">
+       <span>推荐</span>
+     </router-link>
+     <router-link tag="div" class="item" to="/singer">
+       <span>歌手</span>
+     </router-link>
+     <router-link tag="div" class="item" to="/rank">
+       <span>排行</span>
+     </router-link>
+     <router-link tag="div" class="item" to="/search">
+       <span>搜索</span>
+     </router-link>
+   </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Tabbar'
+
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/css/mixin.scss";
+  @import "../assets/css/variable.scss";
+  .tabbar{
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 84px;
+    @include bg_sub_color();
+    .item{
+      span{
+        line-height: 84px;
+        @include font_size($font_medium);
+        @include font_color()
+      }
+      &.router-link-active{
+        span{
+          @include font_active_color()
+        }
+        border-bottom: 5px solid #000000;
+        @include border_color()
+      }
+    }
+  }
+</style>
